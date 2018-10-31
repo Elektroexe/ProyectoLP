004bf5e1a36255ffd83ff67f219feced86d72755
Eduardo <gerson.e.ramirez@gmail.com>
2018-10-31 17:10:06 +0100

    Revert "Funcion Sort cout y merge"
    This reverts commit 52a64aa5ee2f9e6cebcdb3b9096730ec1a3aa414.

--- ProyectoLP/MatriuDispersa.cpp
+++ ProyectoLP/MatriuDispersa.cpp
@@ -1,7 +1,4 @@
 #include "MatriuDispersa.h"
-#include <fstream>
-#include <iostream>
-#include <sstream>
 //#include "MatriuDispersa.h"
 //#include <string>
 //using namespace std;
@@ -202,121 +199,65 @@
 
 MatriuDispersa::MatriuDispersa()
 {
-	maxRow = 0;
-	maxCol = 0;
 }
 
 MatriuDispersa::MatriuDispersa(string fitxert)
 {
-	maxRow = 0;
-	maxCol = 0;
-	ifstream myReadFile;
-	myReadFile.open(fitxert);
-	string line="";
-	int sizeCount = 0;
-	if (myReadFile.is_open())
-	{
-		if (!myReadFile.eof())
-		{
-			while (getline(myReadFile, line)) {
-			
-			
-				stringstream ss(line);
-
-			
-				bool primerVec = true;
-				while (getline(ss, line, '\t')) {
-					if (line != "")
-					{
-						if (primerVec)
-						{
-							rowVector.emplace_back(stoi(line));
-							primerVec = false;
-							valVector.emplace_back(1);
-							if (stoi(line) > maxRow + 1)
-							{
-								maxRow = stoi(line) + 1;
-							}
-
-						}
-						else {
-							colVector.emplace_back(stoi(line));
-							if (stoi(line) > maxCol + 1)
-							{
-								maxCol = stoi(line) + 1;
-							}
-						}
-					}
-			
-					
-				}
-
-			}
-		}
-		
-	}
-	if (maxRow < maxCol)
-	{
-		maxRow = maxCol;
-	} 
-	else
-	{
-		maxCol = maxRow;
-	}
 }
 
 void MatriuDispersa::setVal(const int & nFiles, const int & nColumnes, const int & valor)
 {
-	if (valor > 0)
-	{
-
-		bool trobat = false;
-		for (int i = 0; i < rowVector.size(); i++)
-		{
-			if (rowVector.at(i) == nFiles && colVector.at(i) == nColumnes)
-			{
-				valVector.at(i) = valor;
-				trobat = true;
-
-
-			}
-		}
-		if (!trobat)
-		{
-			rowVector.emplace_back(nFiles);
-			colVector.emplace_back(nColumnes);
-			valVector.emplace_back(valor);
-		
-			if (nFiles > maxRow + 1)
-			{
-				maxRow = nFiles + 1;
-			}
-			if (nColumnes > maxCol + 1)
-			{
-				maxCol = nColumnes + 1;
-			}
-			if (maxRow < maxCol)
-			{
-				maxRow = maxCol;
-			}
-			else
-			{
-				maxCol = maxRow;
-			}
-		}
-	}
-	
-	else
-	{
-		maxCol = maxRow;
-	}
 }
 
 void MatriuDispersa::setVal(const int & valor)
 {
 }
 
+void MatriuDispersa::sortVectors() {
+	int valorRow = 0;
+	int valorCol = 0;
+	int valorVal = 0;
+	for (int a = 0; a < colVector.size() - 1; a++) {
+		for (int b = 0; b < colVector.size() - (a + 1); b++) {
+			if ((rowVector.at(b) > rowVector.at(b + 1)) ||
+				(rowVector.at(b) == rowVector.at(b + 1) && colVector.at(b) > colVector.at(b + 1))) {
+				valorRow = rowVector.at(b + 1);
+				valorCol = colVector.at(b + 1);
+				valorVal = valVector.at(b + 1);
+				rowVector.at(b + 1) = rowVector.at(b);
+				colVector.at(b + 1) = colVector.at(b);
+				valVector.at(b + 1) = valVector.at(b);
+				rowVector.at(b) = valorRow;
+				colVector.at(b) = valorCol;
+				valVector.at(b) = valorVal;
+			}
+		}
+	}
+}
+
 ostream & operator<<(ostream & out, const MatriuDispersa & md)
 {
-	// TODO: insertar una instrucción return aquí
+	int pos = 0;
+	int posRow = md.rowVector.at(pos);
+	int posCol = md.colVector.at(pos);
+	int posVal = md.valVector.at(pos);
+	for (int row = 0; row < md.maxRowCol; row++) {
+		out << "\n ";
+		for (int col = 0; col < md.maxRowCol; col++) {
+			if (row == posRow && col == posCol) {
+				out << md.valVector.at(pos) << "   ";
+				if (pos < (md.rowVector.size() - 1)) {
+					pos++;
+					posRow = md.rowVector.at(pos);
+					posCol = md.colVector.at(pos);
+					posVal = md.valVector.at(pos);
+				}
+			}
+			else {
+				out << "0   ";
+			}
+		}
+		out << "\n";
+	}
+	return out;
 }
